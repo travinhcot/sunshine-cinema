81e5c6acf0e7c57aaf000230e1805ef2f3a13855
phuc_huynh_tan <richardhuynh0706@gmail.com>
2026-06-16 22:55:58 +0700

    adding component for categories page

---
+++ app/categories/categories.tsx
@@ -0,0 +1,4 @@
+'use client'
+import { useState, useEffect } from "react";
+import {Navbar} from "/Users/jacee/Documents/sunshine-cinema-1/component/Navbar";
+import {MovieCard} from "/component/MovieCard.tsx";
---
+++ component/Navbar.tsx
@@ -0,0 +1,4 @@
+'use client'
+import Link from 'next/link';
+import { useState } from 'react';
+import {useAuth} from '@/lib'
